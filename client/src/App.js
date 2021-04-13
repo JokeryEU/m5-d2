a6b9038ab96123a64a5aa83f0c33c7b739788f7f
Mihai Ivanov <mikee2988@yahoo.com>
2021-04-13 18:05:05 +0300

    init commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Students from "./components/Addstudents";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Students />
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- server/src/server.js
+++ server/src/server.js
@@ -7,9 +7,8 @@ const server = express();
 const port = 3001;
 
 server.use(cors());
-server.use(express.json()); // we need to specify this line of code otherwise all the req bodies will be undefined. This line must come BEFORE the routes
+server.use(express.json());
 server.use("/students", studentsRoutes);
-// server.use("/books", booksRoutes)
 
 console.log(listEndpoints(server));
 server.listen(port, () => {
